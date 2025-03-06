36ce5b98dd4ecdfbecbc48da29bfdd7a71eea2a1
Yigit Uyanik <yigit.uyanik@gmail.com>
2025-03-06 21:49:36 +0300

    added python uv setup

--- lua/kickstart/plugins/debug.lua
+++ lua/kickstart/plugins/debug.lua
@@ -145,7 +145,7 @@ return {
         -- See https://github.com/leoluz/nvim-dap-go/blob/main/README.md#configuring
         detached = vim.fn.has 'win32' == 0,
       },
-      require('dap-python').setup(),
+      require('dap-python').setup 'uv',
     }
   end,
 }
